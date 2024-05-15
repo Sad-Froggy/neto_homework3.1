da546e4419b417c3d882803af01291c77faa32b6
Alexey <sepens@yandex.ru>
2024-05-15 13:10:30 +0300

    rm static modifier

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
 
-    public static int calculate(int ticketPrice) {
+    public int calculate(int ticketPrice) {
         int bonusMileCost = 20;
         return ticketPrice / bonusMileCost;
     }
